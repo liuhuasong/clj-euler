ab32bf2ec640f99dd85b1cbe44d77597cd69b4bb
Stefan Oehme <st.oehme@gmail.com>
2012-03-09 18:41:30 +0100

    Problem14 faster and Problem25 more concise

--- src/clj_euler/problems/problem14.clj
+++ src/clj_euler/problems/problem14.clj
@@ -9,13 +9,4 @@
 	    (even? n) (recur (/ n 2) (inc acc))
 	    (odd? n)  (recur (inc (* 3 n)) (inc acc)))))))
 
-(defn my-max-key
-  "Returns the x for which (k x), a number, is greatest.
-   This is a more efficient version than defined in clojure.core"
-  ([k x] x)
-  ([k x y] (if (> (k x) (k y)) x y))
-  ([k x y & more]
-     (second (reduce (fn [x y] (if (> (first x) (first y)) x y))
-                     (map #(vector (k %) %) (cons x (cons y more)))))))
-
-(def problem14 (apply my-max-key #(collatz %) (range 500001 1000000 2)))
+(def problem14 (first (sort-by #(collatz %) > (range 500001 1000000 2))))
--- src/clj_euler/problems/problem25.clj
+++ src/clj_euler/problems/problem25.clj
@@ -1,5 +1,5 @@
 (ns clj-euler.problems.problem25
   (:use [clojure.contrib.lazy-seqs :only (fibs)])
-  (:use [clj-euler.math :only (num-digits)]))
+  (:use [clojure.contrib.math :only (expt)]))
 
-(def problem25 (count (take-while #(< (num-digits %) 1000) (fibs))))
+(def problem25 (count (take-while #(< % (expt 10 999)) (fibs))))
